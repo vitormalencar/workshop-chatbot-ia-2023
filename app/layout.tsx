d5a2839b9df5f2711d4e61ab437826a5a66ba369
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-12-19 11:09:12 -0300

    chore: adding og data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "J.A.R.V.I.S 🤖",
   description: "Chatbot utilizando Next.js TailwindCSS e Vercel IA SKD",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://workshop-chatbot-ia.vercel.app/",
+    siteName: "J.A.R.V.I.S 🤖",
+    images: [
+      {
+        url: "https://workshop-chatbot-ia.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "J.A.R.V.I.S 🤖",
+      },
+    ],
+  },
 };
 
-export default function RootLayout({children}: { children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
